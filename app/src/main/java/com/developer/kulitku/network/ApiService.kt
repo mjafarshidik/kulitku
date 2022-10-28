42f2a8af9389d1f7836ad1402f04b96264ee28d7
arasopraza <aras.opraza@gmail.com>
2022-10-28 08:12:20 +0700

    Initial project structure

---
+++ app/src/main/java/com/developer/kulitku/network/ApiService.kt
@@ -0,0 +1,4 @@
+package com.developer.kulitku.network
+
+interface ApiService {
+}
--- app/src/main/java/com/developer/kulitku/ui/home/HomeActivity.kt
+++ app/src/main/java/com/developer/kulitku/ui/home/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.developer.kulitku
+package com.developer.kulitku.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.developer.kulitku.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
