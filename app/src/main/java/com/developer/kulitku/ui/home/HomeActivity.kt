a956797c0ca09ffc176ff021b5d63f8c4b699f80
arasopraza <aras.opraza@gmail.com>
2022-11-16 01:35:54 +0700

    refactor: change color bottom navigation view

--- app/src/main/java/com/developer/kulitku/ui/home/HomeActivity.kt
+++ app/src/main/java/com/developer/kulitku/ui/home/HomeActivity.kt
@@ -31,6 +31,7 @@ class HomeActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        navView.background = null
         navView.menu.getItem(2).isEnabled = false
 
         supportActionBar?.hide()
