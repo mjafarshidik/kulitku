efc821cd91b8b956b37dba0b02d6b8225410abdb
arasopraza <aras.opraza@gmail.com>
2022-11-15 19:30:48 +0700

    feat: slicing profile

---
+++ app/src/main/java/com/developer/kulitku/ui/profile/ProfileFragment.kt
@@ -0,0 +1,36 @@
+package com.developer.kulitku.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.developer.kulitku.R
+import com.developer.kulitku.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding : FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+        mAuth = Firebase.auth
+
+    }
+
+}
